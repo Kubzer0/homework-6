e5ebbe10940db55182bc869343b23aa9d16ef071
kubzer0 <jakub.gryka@gmail.com>
2018-12-09 17:10:24 +0100

    authentication added

--- src/store.js
+++ src/store.js
@@ -1,17 +1,17 @@
 
 import { createStore, combineReducers, compose, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-
+import auth from './state/auth'
 
 const reducer = combineReducers({
-//@TODO remeber to import reducers later
+    auth
 })
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 export const store = createStore(
-  reducer,
-  composeEnhancers(
-    applyMiddleware(thunk)
-  )
+    reducer,
+    composeEnhancers(
+        applyMiddleware(thunk)
+    )
 )
